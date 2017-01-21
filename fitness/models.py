ef6550d1745582c3f01d15f013353fe4ef68216d
Rob Cardy <rob.cardy.31@gmail.com>
2017-01-21 12:15:07 -0500

    More files!

--- fitness/models.py
+++ fitness/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
+class Member(models.Model):
+    name = models.CharField(max_length=30)
+    email = models.EmailField
+    phone_number = models.
+    age
+    height
+    weight
