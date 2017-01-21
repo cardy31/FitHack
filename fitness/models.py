c47ac57cfe1c58b7b6c7d0d69913ab48a9a51ca5
Rob Cardy <rob.cardy.31@gmail.com>
2017-01-21 12:30:46 -0500

    More files!

--- fitness/models.py
+++ fitness/models.py
@@ -1,11 +1,19 @@
 from django.db import models
+from django.core.validators import RegexValidator
 
 
 # Create your models here.
 class Member(models.Model):
     name = models.CharField(max_length=30)
     email = models.EmailField
-    phone_number = models.
-    age
-    height
-    weight
+    phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$',
+                                 message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
+    phone_number = models.CharField(validators=[phone_regex], blank=True)  # validators should be a list
+    age = models.IntegerField
+    height = models.IntegerField
+    weight = models.IntegerField
+
+class Food(models.Model):
+    name = models.CharField(max_length=50)
+    calories = models.IntegerField()
+    datetime = models.DateTimeField()
